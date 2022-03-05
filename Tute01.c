1e533c298f0834416ba2b45a1f2e083df3d4a45b
IT21176838 <99307615+IT21176838@users.noreply.github.com>
2022-03-05 06:31:50 +0000

    I have finished the tutorial 01.

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,14 @@
 #include <stdio.h>
 
 int main() {
-  
+  int mrk1,mrk2;
+  float avg;
+  printf("Enter yor mark1:");
+  scanf("%d",& mrk1);
+    printf("Enter yor mark2:");
+  scanf("%d",& mrk2);
+  avg=(mrk1+mrk2)/2;
+  printf("Average of the subjects:%.2f",avg);
   return 0;
 }
 
